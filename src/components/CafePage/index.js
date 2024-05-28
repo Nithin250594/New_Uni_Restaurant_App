e30d4461e5d7d408b4238d36e2a1815b3c728ee7
Nithin250594 <nithinsai2505@gmail.com>
2024-05-28 10:44:45 +0000

    add Api loading and success status

--- src/components/CafePage/index.js
+++ src/components/CafePage/index.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react'
+import Loader from 'react-loader-spinner'
 import CafeNavBar from '../CafeNavbar'
 import MenuItems from '../MenuItems'
 import './index.css'
@@ -12,23 +13,32 @@ const Category = {
   fast_food: 'Fast Food',
 }
 
+const status = {
+  initial: 'INITIAL',
+  loading: 'LOADING',
+  success: 'SUCCESS',
+}
+
 const CafePage = () => {
   const [cafeData, setCafeData] = useState([])
   const [activeCategory, setCategory] = useState(Category.salads)
+  const [apiStatus, setApiStatus] = useState(status.initial)
 
   useEffect(() => {
+    setApiStatus(status.loading)
     const fetchData = async () => {
       const dishesApiUrl =
         'https://run.mocky.io/v3/72562bef-1d10-4cf5-bd26-8b0c53460a8e'
       const options = {
         method: 'GET',
       }
-
       const response = await fetch(dishesApiUrl, options)
-      const data = await response.json()
-      setCafeData(data)
+      if (response.ok === true) {
+        const data = await response.json()
+        setCafeData(data)
+        setApiStatus(status.success)
+      }
     }
-
     fetchData()
   }, [])
 
@@ -37,6 +47,25 @@ const CafePage = () => {
       ? cafeData[0].table_menu_list.map(e => e.menu_category)
       : []
   // console.log(MenuCategory)
+
+  const loadingView = () => (
+    <div className="loader-container">
+      <Loader type="ThreeDots" color="#3b82f6" height="80" width="80" />
+    </div>
+  )
+
+  const successView = () => <MenuItems activeCategory={activeCategory} />
+
+  const switchCase = () => {
+    switch (apiStatus) {
+      case status.loading:
+        return loadingView()
+      case status.success:
+        return successView()
+      default:
+        return null
+    }
+  }
 
   return (
     <>
@@ -57,7 +86,7 @@ const CafePage = () => {
           </li>
         ))}
       </ul>
-      <MenuItems activeCategory={activeCategory} />
+      {switchCase()}
     </>
   )
 }
